8bdc930b84d1dd30d07fd55f741751e628876273
YessiMG <malaveryess@gmail.com>
2024-09-24 14:38:59 -0500

    YM Solution to the challenge

---
+++ README.md
@@ -0,0 +1,7 @@
+# Inmersión Dev Clase 02 - Control de gastos mensuales
+Durante la clase se explicó un código inicial, que corresponde al primer commit de este repositorio. Al finalizar se planteó el siguiente desafio:
+- Generar un mensaje de alerta cuando se registre un gasto mayor a 150$ dólares.
+- Agregar un nuevo campo donde se pueda colocar una descripción más detallada del gasto.
+- Agregar un botón que permita modificar los gastos registrados.
+
+`#InmersionDevAlura`
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,32 @@
+const initText = 'Agregar Gasto';
+const updateText = 'Actualizar Gasto';
+
 let expenseNameList = [];
+let expenseDescriptionList = [];
 let expenseValueList = [];
+let itemId = null;
+let saveButton = document.getElementById('botonFormulario');
+let cancelButton = document.getElementById('botonCancelar');
 
 //Esta función se usa cuando el usuario da clic en Guardar Gasto
 function clickBoton() {
     let expenseName = document.getElementById('nombreGasto').value;
     let expenseValue = document.getElementById('valorGasto').value;
+    let expenseDescription = document.getElementById('descriptionGasto').value;
 
-    expenseNameList.push(expenseName);
-    expenseValueList.push(expenseValue);
-    
+    if(expenseValue > 150) alert('Este gasto es mayor a 150 USD ¡Ten cuidado!');
+
+    if(itemId != null && itemId != undefined) {
+        expenseNameList[itemId] = expenseName;
+        expenseValueList[itemId] = expenseValue;
+        expenseDescriptionList[itemId] = expenseDescription;
+    }
+    else {
+        expenseNameList.push(expenseName);
+        expenseValueList.push(expenseValue);
+        expenseDescriptionList.push(expenseDescription);
+    }
+
     updateExpenseList();
 }
 
@@ -16,14 +34,17 @@ function updateExpenseList() {
     //Es const ya que no cambia
     const elementList = document.getElementById('listaDeGastos');
     const elementTotal = document.getElementById('totalGastos');
+
     let htmlList = '';
     let expenseTotal = 0;
 
     expenseNameList.forEach((element, position) => {
         const expenseValue = Number(expenseValueList[position]);
+        const expenseDescription = expenseDescriptionList[position];
         htmlList += `<li>
-                ${element} - USD ${expenseValue.toFixed(2)}
+                ${element} - USD ${expenseValue.toFixed(2)} (${expenseDescription})
                 <button onclick="removeExpense(${position});">Eliminar</button>
+                <button onclick="loadUpdate(${position});">Actualizar</button>
             </li>`;
         expenseTotal += Number(expenseValue);
     });
@@ -37,12 +58,26 @@ function updateExpenseList() {
 
 function clean() {
     document.getElementById('nombreGasto').value = '';
+    document.getElementById('descriptionGasto').value = '';
     document.getElementById('valorGasto').value = '';
+    itemId = null;
+    saveButton.innerText = initText;
+    cancelButton.style.display = 'none';
 }
 
 function removeExpense(position) {
     expenseNameList.splice(position, 1);
     expenseValueList.splice(position, 1);
+    expenseDescriptionList.splice(position, 1);
 
     updateExpenseList();
+}
+
+function loadUpdate(position) {
+    itemId = position;
+    document.getElementById('nombreGasto').value = expenseNameList[itemId];
+    document.getElementById('descriptionGasto').value = expenseDescriptionList[itemId];
+    document.getElementById('valorGasto').value = expenseValueList[itemId];
+    saveButton.innerText = updateText;
+    cancelButton.style.display = 'inline';
 }
